75a5803dce54a3b10af26b7b58d3e3d5e987a681
arajali123 <arajali.sds@gmail.com>
2025-09-29 14:44:37 +0600

    first tabs menu add done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
 
 // Tabs JS
 document.addEventListener("DOMContentLoaded", function () {
-  const tabLinks = document.querySelectorAll(".tabs-mountain-main-wrapper .tab-button");
-  const tabBodies = document.querySelectorAll(".tabs-mountain-main-wrapper .tabs-mountain-main-wrap");
+  const tabLinks = document.querySelectorAll(".tabs-v2-main-wrapper .tab-button");
+  const tabBodies = document.querySelectorAll(".tabs-v2-main-wrapper .tabs-v2-mountain-main-wrap");
 
   tabLinks.forEach((tabLink) => {
     tabLink.addEventListener("click", function (e) {
       e.preventDefault();
       e.stopPropagation();
 
-      // Remove Active from all tabs and bodies
       tabLinks.forEach((link) => link.classList.remove("active"));
       tabBodies.forEach((body) => body.classList.remove("active"));
 
-      // Add Active to clicked tab
       this.classList.add("active");
 
       const targetId = this.getAttribute("href");
